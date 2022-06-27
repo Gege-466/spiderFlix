ad1fae7341e31b3d2bbaafc0d3dd1cb6f0b9da15
geovanna lima <lsgeovanna072@gmail.com>
2022-06-27 07:48:58 -0300

    coloquei uma foto de fundo, um campo para preencher, um botao de busca e uma lista de filmes

---
+++ index.js
@@ -0,0 +1,21 @@
+function buscarFilme() {
+    let campoFilme = document.getElementById("filme").value;
+    if (campoFilme == "") {
+        alert("O campo esta vazio!! Digite algo")
+    }
+}
+
+
+    const filmes = ["https://upload.wikimedia.org/wikipedia/pt/thumb/1/14/Spide-Man_Poster.jpg/250px-Spide-Man_Poster.jpg", 
+    "https://br.web.img2.acsta.net/pictures/210/544/21054460_2013103118041242.jpg",
+    "https://upload.wikimedia.org/wikipedia/pt/3/37/Spider-Man_3.jpg",
+    "https://upload.wikimedia.org/wikipedia/pt/thumb/5/53/The_Amazing_Spider-Man_Poster.jpg/250px-The_Amazing_Spider-Man_Poster.jpg",
+    "https://static.wikia.nocookie.net/dublagem/images/4/43/OEspetacularHomemAranha2.jpg/revision/latest?cb=20211005141748&path-prefix=pt-br",
+    "https://br.web.img3.acsta.net/pictures/17/05/29/23/31/530814.jpg", 
+    "https://br.web.img2.acsta.net/pictures/19/07/05/17/30/5167951.jpg", 
+    "https://static.wikia.nocookie.net/universocinematograficomarvel/images/3/38/Homem-Aranha_-_Sem_Volta_Para_Casa_-_P%C3%B4ster_Nacional_-_03.jpg/revision/latest?cb=20211127230334&path-prefix=pt"]
+    
+    for (var i = 0; i < filmes.length; i++) {
+        document.write("<img src=" + filmes[i] + ">")
+    }
+    
